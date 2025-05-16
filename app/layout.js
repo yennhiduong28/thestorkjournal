b6fee37157d363be99407d9399f3c5e13a361984
Bao Nguyen <baonguyen@Baos-MacBook-Air-3.local>
2025-05-16 17:34:33 +0700

    fixed title

--- app/layout.js
+++ app/layout.js
@@ -19,6 +19,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head> 
+        <title>The Stork Journal</title>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
